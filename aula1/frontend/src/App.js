41cf5eb5e239ce6847c3c27f08823ffd784acc89
josemalcher <contato@josemalcher.net>
2020-03-28 19:36:21 -0300

    3 - 25/03- Construindo a interface web  - REACT    - Estado    - Imutabilidade

--- aula1/frontend/src/App.js
+++ aula1/frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./Header";
 
 function App() {
+    //let counter = userState(0); // retorna Array [valor, FuncaoDeAtualizacao]
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
     return (
-        //<Header title="Semana OmnisStack"/>
-        <Header>Semana Omnistack</Header>
+        <div>
+            <Header>Contador {counter}</Header>
+            <button onClick={increment}>Incrementar</button>
+        </div>
     );
 }
 

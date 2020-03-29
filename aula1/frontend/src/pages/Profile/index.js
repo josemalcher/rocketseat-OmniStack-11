c601190c01ebf6feba351bb82c88a34c05e426f5
josemalcher <contato@josemalcher.net>
2020-03-29 00:51:02 -0300

    3 - 25/03- Construindo a interface web - Front Página de Profile com back

--- aula1/frontend/src/pages/Profile/index.js
+++ aula1/frontend/src/pages/Profile/index.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiPower, FiTrash2 } from 'react-icons/fi'; // Ícone de energia e lixo.
+import React, { useState, useEffect } from 'react'; // * useEffect.
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId'); // Recuperará o id da ONG.
+    const ongName = localStorage.getItem('ongName'); // Recuperará o nome da ONG.
+
+    useEffect(() => {
+        api.get('profile', { // * Recuperação dos dados vindo da rota /profile.
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    }, [ongId]);
+
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        } catch (err) {
+            alert('Erro ao deleter caso, tente novamente.');
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear(); // Limpa o localStorage.
+
+        history.push('/'); // Volta para a página de login.
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
@@ -22,63 +61,51 @@ export default function Profile() {
             <h1>Casos cadastrados</h1>
 
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.title}</p>
 
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incident.description}</p>
 
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
+                        <strong>VALOR:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'})
+                            .format(incident.value)}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
+                        <button
+                            onClick={() => handleDeleteIncident(incident.id)}
+                            type="button"
+                        >
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
 }
+
+/*
+* ** useEffect* - Dispara uma função em um determinado evento do componente, como por exemplo, assim que é mostrado em tela.
+
+Recebe dois parâmetros - o primeiro é qual função queremos que seja executada.
+O segundo é quando a função vai ser executada, e será um array de dependências. Toda vez que as informações que estiverem dentro desse array mudarem, ele executará a função de novo.
+
+Por exemplo, se temos um `useEffect(() => {}, [ongName]);`, toda vez que o valor de *ongName* mudar, ele executará a função do primeiro parâmetro. Se deixarmos o *array* vazio, ele executará apenas uma única vez no fluxo do nosso componente.
+
+** Recuperação dos dados vindo da rota /profile* - Da rota *profile*, será levado o *header* de *autorization* que é o *ongId* que estava armazenado no *localStorage*. Depois disso, pegamos a *response* e precisamos armazená-la em algum lugar, que seria o estado, o *incidents*. Utilizamos o *setIncidents* porque este que é o responsável por alterar o valor da variável. O *ongId* que está em um *array* no final não é obrigatório para a atual situação, porém é uma boa prática já que estamos utilizando o *React*.
+
+
+** Deletará o incident* - Receberá o *id* como parâmetro, e é de acordo com este *id* que será deletado o *incident*, mas também pegando o *header* de autorização.
+
+** Filter* - Se tudo estiver certo para ser deletado, o caso será deletado mas não será excluído de nossa interface, só se recarregarmos a página. Resolvemos isso utilizando o *filter*, vamos fazer uma varredura em nosso *array* de *incidents*, procurar o que tem o *id* passado e remover ele de dentro.
+
+Traduzindo - `setIncidents(incidents.filter(incident => [incident.id](http://incident.id/) !== id));` Pegamos todos os *incidents* que já temos e vamos fazer um filtro neles: para cada um dos *incidents*, queremos manter apenas os *incidents* em que o *id* for diferente daquele que deletamos.
+
+** Formato da moeda brasileira* - Utilizamos o método *NumberFormat* de *Intl* que já vem por padrão com o *JavaScript*. Passamos que a língua é pt-br, que queremos o tipo moeda (*currency)* e que o formato de moeda é o BRL, que é o padrão brasileiro de moeda. Colocamos o *`.format`* dizendo o que queremos formatar, que no caso é o valor do caso, *incident.value*.
+
+** Botão delete* - O que estamos passando para o *onClick* é uma função e não o retorno de uma função, por isso utilizamos o *arrow function*.
+*
+*
+* */
